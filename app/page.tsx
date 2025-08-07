f5947fc78526a8d9e95d98abe8ba2152df3bc5f6
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-08-07 23:10:50 +0530

    Aligned userinfomration on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import UserInformation  from "@/components/UserInformation";
 
 export default function Home() {
   return (
-      <div className="grid">
-        <section>
+      <div className="grid grid-cols-8 mt-5 sm:px-5">
+        <section className="hidden md:inline md:col-span-2">
           <UserInformation />
         </section>
 
--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -45,6 +45,16 @@ async function UserInformation() {
 				</Button>
 			</div>
 		</SignedOut>
+		<hr  className="w-full border-gray-200 my-5"/>
+
+		<div className="flex justify-between w-full px-4 text-sm">
+			<p className="font-semibold text-gray-400">Posts</p>
+			<p className="text-blue-400">0</p>
+		</div>
+		<div className="flex justify-between w-full px-4 text-sm">
+			<p className="font-semibold text-gray-400">Comments</p>
+			<p className="text-blue-400">0</p>
+		</div>
 	</div>
   );
 }
