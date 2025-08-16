3039bd5f812d61ffcb87f844c0a44bfb7e92dc75
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-08-16 23:29:21 +0530

    Start with actions

---
+++ actions/createPostAction.ts
@@ -0,0 +1,31 @@
+'use server'
+
+import { currentUser } from "@clerk/nextjs/server"
+
+export default async function createPOstAction(formData: FormData) {
+	const user = await currentUser()
+	
+	if (!user) {
+		throw new Error("USer not authenticated")
+	}
+
+	const postInput = formData.get("postInput") as string;
+	const image = formData.get("image") as File;
+	let imageUrl: string | undefined;
+
+	if (!postInput) {
+		throw new Error("post input is required");
+	}
+
+	// define user
+
+	// upload image if there is one
+
+	// create post in database
+
+
+	// revalidatePath '/' - home pages
+ 
+
+
+}
