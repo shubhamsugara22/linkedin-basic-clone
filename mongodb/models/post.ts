779620d54718529da66eb9e18bf93b5bfb8fd977
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-08-19 23:14:46 +0530

    Adding model for posts

--- mongodb/models/post.ts
+++ mongodb/models/post.ts
@@ -14,3 +14,20 @@ export interface IPost extends IPostBase, Document {
 	createdAt : Date;
 	updatedAt: Date; 
 }
+
+// Define the document methods ( for each instance of a post)
+interface IPostMethods {
+	likePost(userId: string): Promise<void>;
+	unlikePost(userId: string): Promise<void>;
+	commentOnPost(comment: ICommentBase): Promise<void>;
+	getAllComments(): Promise<IComment[]>;
+	removePost(): Promise<void>;
+}
+interface IPostStatics {
+	getAllPosts(): Promise<IPostDocument[]>;
+}
+
+export interface IPostDocument extends IPost , IPostMethods {} // Singular instace of a post
+interface IPostModel extends IPostStatics, Model<IPostDocument> {} // all posts
+
+const PostSchema = new Schema<IPostDocument>()
