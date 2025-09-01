a9eca70e6cd5302b9e582fe06b2f28506f4dee4f
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-09-01 23:51:25 +0530

    Starting with GET api route

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,4 +1,5 @@
 import { IUser } from "@/types/user";
+import { NextResponse } from "next/server";
 
 export interface AddPostRequestBody {
 	user: IUser;
@@ -11,6 +12,15 @@ export async function POST(request: Request) {
 }
 
 export async function GET(request: Request) {
+	try {
+
+	} catch (error) {
+		return NextResponse.json(
+			{ error:  "An error occurred while fetching posts" },
+			{ status: 500 }
+		);
+
+	}
 
 	
 }
