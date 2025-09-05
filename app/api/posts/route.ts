d08216796e8c5a5dc8f7a86d97c1ec594f22f753
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-09-05 23:26:26 +0530

    Add api routes in posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,8 @@
 import { IUser } from "@/types/user";
 import connectDB from "@/mongodb/db";
 import { NextResponse } from "next/server";
+import { IPostBase ,Post } from "@/mongodb/models/post";
+import { auth } from '@clerk/nextjs/server'
 
 export interface AddPostRequestBody {
 	user: IUser;
@@ -8,13 +10,33 @@ export interface AddPostRequestBody {
 	imageUrl?: string | null ;
 }
 
-export async function POST(request: Request) {
+export async function POST(request: Request) {(
+	auth.protect();
 
+	try {
+		await connectDB();
+		const { user , text, imageUrl } : AddPostRequestBody = await request.json();
+	    const postData: IPostBase = {
+		    user,
+		    text,
+		    ...(imageUrl && { imageUrl }),
+	};
+	const post = await Post.create(postData);
+	return NextResponse.json({ message: "Post created Successfully", post});
+	} catch (error) {
+		return NextResponse.json(
+			{ error:  "An error occurred while creating the post" },
+			{ status: 500 }
+		);
+	}
 }
-
 export async function GET(request: Request) {
 	try {
-		await connectDB()
+		await connectDB();
+
+		const posts = await Post.getAllPosts();
+
+		return NextResponse.json({ posts});
 
 	} catch (error) {
 		return NextResponse.json(
